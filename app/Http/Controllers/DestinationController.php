d5567721ecc6f978342913cc99e830d727fdfe6e
sunnytechie <sunnyict001@gmail.com>
2023-05-18 23:02:53 +0100

    destination added

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class DestinationController extends Controller
 {
@@ -11,7 +13,8 @@ class DestinationController extends Controller
      */
     public function index()
     {
-        //
+        $destinations = Destination::orderBy('id', 'desc')->get();
+        return view('dashboard.destination.index', compact('destinations'));
     }
 
     /**
@@ -19,7 +22,7 @@ class DestinationController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.destination.new');
     }
 
     /**
@@ -27,7 +30,27 @@ class DestinationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate
+        $request->validate([
+            'price_range' => 'required',
+            'state' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        //dd($request->all());
+        
+        $imagePath = request('image')->store('projects', 'public');
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(540, 675);
+        $image->save();
+
+        $destination = new Destination();
+        $destination->price_range = $request->price_range;
+        $destination->state = $request->state;
+        $destination->image = $imagePath;
+        $destination->save();
+
+        return redirect()->route('destinations.index')->with('message', "You have successfully posted a new destination sample.");
+
     }
 
     /**
@@ -59,6 +82,8 @@ class DestinationController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $destination = Destination::find($id);
+        $destination->delete();
+        return redirect()->route('destinations.index')->with('message', "You have successfully deleted a destination reel.");
     }
 }
