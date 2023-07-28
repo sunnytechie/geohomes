5b98befd0b15226c887778602b8361be68cfca34
sunnytechie <sunnyict001@gmail.com>
2023-07-28 21:58:52 +0100

    new

--- resources/views/snippets/home/welcomeslide.blade.php
+++ resources/views/snippets/home/welcomeslide.blade.php
@@ -1,27 +1,26 @@
 <section>
-    <div class="slick-slider mx-0 custom-arrow-center"
-     data-slick-options='{"slidesToShow": 1, "autoplay":true, "autoplaySpeed": 10000, "responsive":[{"breakpoint": 1200,"settings": {"slidesToShow":1,"arrows":false,"dots":false}},{"breakpoint": 992,"settings": {"slidesToShow":1,"arrows":false,"dots":false}},{"breakpoint": 768,"settings": {"slidesToShow": 1,"arrows":false,"dots":false}},{"breakpoint": 576,"settings": {"slidesToShow": 1,"arrows":false,"dots":false}}]}'>
-      <div class="box px-0 d-flex flex-column">
-        <div class="bg-cover d-flex align-items-center custom-vh-02"
-             style="background-image: url('assets/images/bg-slider-01.jpg')">
-          <div class="container">
+    <div class="slick-slider mx-0 custom-arrow-center" data-slick-options='{"slidesToShow": 1, "autoplay":true, "autoplaySpeed": 10000, "responsive":[{"breakpoint": 1200,"settings": {"slidesToShow":1,"arrows":false,"dots":false}},{"breakpoint": 992,"settings": {"slidesToShow":1,"arrows":false,"dots":false}},{"breakpoint": 768,"settings": {"slidesToShow": 1,"arrows":false,"dots":false}},{"breakpoint": 576,"settings": {"slidesToShow": 1,"arrows":false,"dots":false}}]}'>
+        {{-- <div class="box px-0 d-flex flex-column">
+            <div class="bg-cover d-flex align-items-center custom-vh-02"
+                style="background-image: url('assets/images/bg-slider-01.jpg')">
+            <div class="container">
 
-            <div class="spacer mt-8"></div>
+                <div class="spacer mt-8"></div>
 
-            <div class="row py-8" data-animate="zoomIn">
-                <div class="col-lg-5 col-md-6 d-md-block d-none">
-                    <a href="#" class="d-inline-block"><img src="{{ asset('assets/images/single-image-05.png') }}" class="rounded-lg" alt="Villa on Hollywood Boulevard"></a>
+                <div class="row py-8" data-animate="zoomIn">
+                    <div class="col-lg-5 col-md-6 d-md-block d-none">
+                        <a href="#" class="d-inline-block"><img src="{{ asset('assets/images/single-image-05.png') }}" class="rounded-lg" alt="Villa on Hollywood Boulevard"></a>
+                    </div>
+
+                    <div class="col-lg-7 col-md-6">
+                    <h2 class="lh-12 mb-4 mt-8"><a href="#" class="text-white fs-md-60 fs-48">The GeoHomes <br> Masterpiece.</a></h2>
+                    <p class="text-white fs-18 font-weight-500">Geohomes Services Limited is a leading real estate development and consulting firm. </p>
+                    </div>
+
                 </div>
-
-                <div class="col-lg-7 col-md-6">
-                  <h2 class="lh-12 mb-4 mt-8"><a href="#" class="text-white fs-md-60 fs-48">The GeoHomes <br> Masterpiece.</a></h2>
-                  <p class="text-white fs-18 font-weight-500">Geohomes Services Limited is a leading real estate development and consulting firm. </p>
                 </div>
-
-              </div>
             </div>
-          </div>
-        </div>
+        </div> --}}
         <div class="box px-0 d-flex flex-column">
           <div class="bg-cover d-flex align-items-center custom-vh-02"
              style="background-image: url('assets/images/bg-slider-02.jpg')">
@@ -33,7 +32,7 @@
                 <div class="col-lg-5 col-md-6 d-md-block d-none">
                   <a href="#" class="d-inline-block"><img src="{{ asset('assets/images/single-image-06.jpg') }}" class="rounded-lg" alt="Villa on Hollywood Boulevard"></a>
                   </div>
-                  
+
                   <div class="col-lg-7 col-md-6">
                     <h2 class="lh-12 mb-4 mt-8"><a href="#" class="text-white fs-md-60 fs-48">Recreating your <br> dream.</a></h2>
                     <p class="text-white fs-18 font-weight-500">We take the position of a customer-focused, and solution-driven company eager to provide what our clients need to achieve their dreams.</p>
@@ -42,27 +41,27 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="box px-0 d-flex flex-column">
+        </div>
+        <div class="box px-0 d-flex flex-column">
             <div class="bg-cover d-flex align-items-center custom-vh-02"
              style="background-image: url('assets/images/bg-slider-03.jpg')">
               <div class="container">
 
                 <div class="spacer mt-8"></div>
-                
+
                 <div class="row py-8" data-animate="zoomIn">
                   <div class="col-lg-5 col-md-6 d-md-block d-none">
                     <a href="#" class="d-inline-block"><img src="{{ asset('assets/images/single-image-07.jpg') }}" class="rounded-lg" alt="Villa on Hollywood Boulevard"></a>
                   </div>
-                    
+
                   <div class="col-lg-7 col-md-6">
                     <h2 class="lh-12 mb-4 mt-8"><a href="#" class="text-white fs-md-60 fs-48">From design to <br> reality.</a></h2>
                     <p class="text-white fs-18 font-weight-500">Our work and services pass every global standard and are championed by experts and experienced professionals who are trained to give you the best. </p>
                   </div>
-                    
+
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </section>
+        </div>
+    </div>
+</section>
