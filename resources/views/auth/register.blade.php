4c77a7d303c5d40ea6f37506a53019db07cfc30f
sunnytechie <sunnyict001@gmail.com>
2023-10-12 15:40:56 +0100

    new

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,21 +18,41 @@
     margin-top: 45px;
 }
 
+/* @media 425px */
+@media (max-width: 426px) {
+    .card {
+        width: 80%;
+        margin: 0 auto;
+    }
+    .regImg {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    margin: 0 auto;
+}
+
+.sm-mb-4 {
+    margin-bottom: 1.5rem!important;
+}
+}
+
 
 </style>
 <section class="py-2">
     <div class="container">
         <div class="row justify-content-center login-register">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <a href="/">
                     <img height="100px" width="180px" src="{{ asset('assets/images/logo/geohomeslogo.png') }}" alt="">
                 </a>
                 <p class="mb-4" style="color: #fff; font-weight:600">Choose a signup option.</p>
             </div>
         </div>
+
         {{-- <div class="mt-9 hide-from-1024"></div> --}}
         <div class="row justify-content-center mb-7">
-        <div class="col-md-3 col-6">
+
+        <div class="col-sm-3 col-12 sm-mb-4">
             <a href="{{ route('auth.customer') }}">
             <div class="card border-1 shadow">
                 <img class="regImg" src="{{ asset('assets/images/svg/customer.png') }}" alt="">
@@ -41,7 +61,7 @@
             </a>
         </div>
 
-        <div class="col-md-3 col-6">
+        <div class="col-sm-3 col-12">
             <a href="{{ route('auth.agent') }}">
             <div class="card border-1 shadow">
                 <img class="regImg" src="{{ asset('assets/images/svg/provider.png') }}" alt="">
